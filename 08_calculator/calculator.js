b593f71d4b8dcd7a01ddbad3853a4edbf56d336d
dreadlord-sedai <dahamifabbio@gmail.com>
2025-01-24 18:04:01 +0530

    Update functions for temperature conversion, sum calculation, leap year determination, array manipulation, Fibonacci sequence, and palindrome checking; enhance test cases and fix Jest version.

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,31 @@
-const add = function() {
+const add = function(a, b) {
+	return a + b;
+};
+
+const subtract = function(a, b) {
+	return a -b;
+};
+
+const sum = function(array) {
+  return array.reduce((total, current) => total + current, 0);
+};
+
+const multiply = function(array) {
+  return array.reduce((product, current) => product * current, 1);
+};
+
+const power = function(a, b) {
+  const result = a ** b;
+  return result;
 	
 };
 
-const subtract = function() {
-	
-};
-
-const sum = function() {
-	
-};
-
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(n) {
+  let product = 1;
+  for (let i = n; i > 0; i--) {
+    product *= i;
+  }
+  return product;
 };
 
 // Do not edit below this line
